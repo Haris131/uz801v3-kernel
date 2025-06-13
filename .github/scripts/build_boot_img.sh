177271982a66ecba3acc689fe32f104e6cea6757
Haris131 <41672454+Haris131@users.noreply.github.com>
2025-06-14 00:33:05 +0700

    Update build_boot_img.sh

--- .github/scripts/build_boot_img.sh
+++ .github/scripts/build_boot_img.sh
@@ -4,9 +4,7 @@ DOWNLOAD_SERVER="images.linuxcontainers.org"
 DOWNLOAD_INDEX_PATH="/meta/1.0/index-system"
 DOWNLOAD_DISTRO="debian;bookworm;arm64;default"
 
-DTB_FILE=msm8916-thwc-ufi003.dtb
-DTB_FILE_NO_MODEM=msm8916-thwc-ufi003-no-modem.dtb
-DTB_FILE_NO_MODEM_OC=msm8916-thwc-ufi003-no-modem-oc.dtb
+DTB_FILE=msm8916-yiming-uz801v3.dtb
 RAMDISK_FILE=initrd.img
 
 rootfs_url="https://$DOWNLOAD_SERVER$(curl -m 10 -fsSL "https://$DOWNLOAD_SERVER$DOWNLOAD_INDEX_PATH" | grep "$DOWNLOAD_DISTRO" | cut -f 6 -d ';')rootfs.tar.xz"
@@ -46,7 +44,7 @@ umount rootfs/dev
 umount rootfs/sys
 cp rootfs/boot/vmlinuz* ./Image.gz
 cp rootfs/boot/initrd.img* ./initrd.img
-cp rootfs/usr/lib/linux-image*/qcom/*ufi003*.dtb ./
+cp rootfs/usr/lib/linux-image*/qcom/*uz801v3*.dtb ./
 
 cat Image.gz $DTB_FILE > kernel-dtb
 mkbootimg \
@@ -59,29 +57,5 @@ mkbootimg \
     --ramdisk "$RAMDISK_FILE" \
     --cmdline "earlycon root=PARTUUID=a7ab80e8-e9d1-e8cd-f157-93f69b1d141e console=ttyMSM0,115200 no_framebuffer=true rw"\
     --kernel kernel-dtb -o boot.img
-
-cat Image.gz $DTB_FILE_NO_MODEM > kernel-dtb
-mkbootimg \
-    --base 0x80000000 \
-    --kernel_offset 0x00080000 \
-    --ramdisk_offset 0x02000000 \
-    --tags_offset 0x01e00000 \
-    --pagesize 2048 \
-    --second_offset 0x00f00000 \
-    --ramdisk "$RAMDISK_FILE" \
-    --cmdline "earlycon root=PARTUUID=a7ab80e8-e9d1-e8cd-f157-93f69b1d141e console=ttyMSM0,115200 no_framebuffer=true rw"\
-    --kernel kernel-dtb -o boot-no-modem.img
-
-cat Image.gz $DTB_FILE_NO_MODEM_OC > kernel-dtb
-mkbootimg \
-    --base 0x80000000 \
-    --kernel_offset 0x00080000 \
-    --ramdisk_offset 0x02000000 \
-    --tags_offset 0x01e00000 \
-    --pagesize 2048 \
-    --second_offset 0x00f00000 \
-    --ramdisk "$RAMDISK_FILE" \
-    --cmdline "earlycon root=PARTUUID=a7ab80e8-e9d1-e8cd-f157-93f69b1d141e console=ttyMSM0,115200 no_framebuffer=true rw"\
-    --kernel kernel-dtb -o boot-no-modem-oc.img
 
 mv boot*.img ../../artifacts/
